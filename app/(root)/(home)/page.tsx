0fe320773572a7161162c6304ab732eadd2314be
ariyannandi <ariyannandi2711@gmail.com>
2024-07-30 16:43:07 +0530

    fixed import errors in home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,7 @@
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
-<<<<<<< Updated upstream
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-=======
-import LocalSearchbar from "@/components/shared/LocalSearchbar";
->>>>>>> Stashed changes
 import NoResults from "@/components/shared/NoResults";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
@@ -21,7 +17,7 @@ const Home = async () => {
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+          <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
             Ask a Question
           </Button>
         </Link>
