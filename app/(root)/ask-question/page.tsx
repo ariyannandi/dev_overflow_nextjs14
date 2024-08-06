72c0eb91f8fb78934fcbf5f1f4274f944a394e3b
ariyannandi <ariyannandi2711@gmail.com>
2024-08-07 02:27:44 +0530

    add optional catch all routes and user creation for ask-questions

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,17 +1,15 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  const userId = "clerk123";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
-
 
   return (
     <div>
