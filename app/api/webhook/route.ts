b92d7763c7d5090e2c3b629dc76eba6832f88826
ariyannandi <ariyannandi2711@gmail.com>
2024-08-07 01:18:57 +0530

    add a default username

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -60,8 +60,8 @@ export async function POST(req: Request) {
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
-      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-      username: username!,
+      name: `${first_name}${last_name ? `${last_name}` : ""}`,
+      username: username || "Userx",
       email: email_addresses[0].email_address,
       picture: image_url,
     });
