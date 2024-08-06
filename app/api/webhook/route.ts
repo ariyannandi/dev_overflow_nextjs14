447edb5da60c307fab7118a5850d78d1d9b79a29
ariyannandi <ariyannandi2711@gmail.com>
2024-08-07 01:29:43 +0530

    fix an error

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,7 +6,6 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -15,29 +14,24 @@ export async function POST(req: Request) {
     );
   }
 
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response("Error occurred -- no svix headers", {
       status: 400,
     });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -46,7 +40,7 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    return new Response("Error occurred", {
       status: 400,
     });
   }
@@ -55,8 +49,8 @@ export async function POST(req: Request) {
 
   console.log("Event Type:", eventType);
 
-  if (eventType === "user.created") {
-    try {
+  try {
+    if (eventType === "user.created") {
       const {
         id,
         email_addresses,
@@ -74,16 +68,9 @@ export async function POST(req: Request) {
       });
       console.log("User created successfully:", mongoUser);
       return NextResponse.json({ message: "OK", user: mongoUser });
-    } catch (error) {
-      console.error("Error creating user:", error);
-      return new Response("Error occurred while creating user", {
-        status: 500,
-      });
     }
-  }
 
-  if (eventType === "user.updated") {
-    try {
+    if (eventType === "user.updated") {
       const {
         id,
         email_addresses,
@@ -104,31 +91,18 @@ export async function POST(req: Request) {
       });
       console.log("User updated successfully:", mongoUser);
       return NextResponse.json({ message: "OK", user: mongoUser });
-    } catch (error) {
-      console.error("Error updating user:", error);
-      return new Response("Error occurred while updating user", {
-        status: 500,
-      });
     }
-  }
 
-  if (eventType === "user.deleted") {
-    try {
+    if (eventType === "user.deleted") {
       const { id } = evt.data;
       const deletedUser = await deleteUser({ clerkId: id! });
       console.log("User deleted successfully:", deletedUser);
       return NextResponse.json({ message: "OK", user: deletedUser });
-    } catch (error) {
-      console.error("Error deleting user:", error);
-      return new Response("Error occurred while deleting user", {
-        status: 500,
-      });
     }
-  }
 
-  return NextResponse.json({ message: "OK" });
-} catch (error) {
-  console.error("Function crashed:", error);
-  return new Response("Internal Server Error", { status: 500 });
-}
+    return NextResponse.json({ message: "OK" });
+  } catch (error) {
+    console.error("Function crashed:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
